521567d69ed6d410791972085c642d6376a38c88
mickedav <mikda221@student.liu.se>
2016-08-01 00:03:15 +0200

    test app

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 angular
   .module('App', [
     'ngRoute',
-
+    'ui.bootstrap'
 
   ])
   .config(function ($routeProvider) {
@@ -11,6 +11,23 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: "Home/home.html"
+    })
+
+    $routeProvider
+      .when('/portfolio', {
+        templateUrl: "/Portfolio/portfolio.html",
+        controller: "portfolioCtrl"
+    })
+
+    $routeProvider
+      .when('/game', {
+        templateUrl: "/Game/game.html",
+    })
+
+    $routeProvider
+      .when('/portfolio/:id', {
+        templateUrl: "/Portfolio/project.html",
+        controller: "projectCtrl"
     })
 
       .otherwise({
